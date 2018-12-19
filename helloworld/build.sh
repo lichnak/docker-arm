58ebee7a9068e61be312d0f979ad433b3f1bb23a
Xabi Ezpeleta <xezpeleta@gmail.com>
2018-12-19 12:35:31 +0100

    Added qemu exec permission

--- helloworld/build.sh
+++ helloworld/build.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
+set -e
 
 image="xezpeleta/helloworld"
-version="1.1"
+version="1.2"
 
 # Register handlers
 # Register qemu-*-static for all supported processors except the current one
 # https://github.com/multiarch/qemu-user-static
 #docker run --rm --privileged multiarch/qemu-user-static:register --reset
 
+# Download a local copy of qemu
+curl -L https://github.com/multiarch/qemu-user-static/releases/download/v3.0.0/qemu-arm-static -o qemu-arm-static
+
 # Build and push images
-for arch in amd64 arm32v7; do
+for arch in arm32v7 amd64; do
   docker build -f Dockerfile.${arch} -t ${image}:${arch}-${version} .
   docker push ${image}:${arch}-${version}
 done
