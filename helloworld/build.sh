f683e3991a8b201b21215e9fb640918f01ae5f4f
Xabi Ezpeleta <xezpeleta@gmail.com>
2018-12-19 08:12:48 +0100

    Docker manifest disabled

--- helloworld/build.sh
+++ helloworld/build.sh
@@ -6,7 +6,7 @@ version="1.1"
 # Register handlers
 # Register qemu-*-static for all supported processors except the current one
 # https://github.com/multiarch/qemu-user-static
-#docker run --rm --privileged multiarch/qemu-user-static:register
+#docker run --rm --privileged multiarch/qemu-user-static:register --reset
 
 # Build and push images
 for arch in amd64 arm32v7; do
@@ -18,9 +18,6 @@ done
 # Build a multi-arch manifest
 docker manifest create ${image}:${version} ${image}:amd64-${version} ${image}:arm32v7-${version}
 docker manifest create ${image}:latest ${image}:amd64-${version} ${image}:arm32v7-${version}
-#RPi
-#docker manifest annotate ${image}:latest ${image}:arm32v6-latest --os linux --arch arm
-#QNAP
-#docker manifest annotate ${image}:latest ${image}:arm32v7 --os linux --arch arm
+
 docker manifest push ${image}:${version}
 docker manifest push --purge ${image}:latest
